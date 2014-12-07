b833e766037da4ff050731a49b7012276e1fd8c1
Ken Newcomb <KenNewcombNJ@gmail.com>
2014-12-07 11:26:05 -0500

    Added atom class + initial program structure

---
+++ simulate.py
@@ -0,0 +1,15 @@
+import sys
+
+## Basic program structure (to be implemented):
+
+# Create 864 atom objects
+
+# Give them random positions and momenta (applying Boltzmann distribution @ T)
+
+# Allow them to move according to some time step
+
+# Integrate, find new positions and momenta
+
+# Repeat until finished.
+
+# Analyze results (create velocity autocorrelation function and from this, compute material properties)
