8afb3721ebf0086fe9bb805f7a5bd26232b3258d
Ken Newcomb <KenNewcombNJ@gmail.com>
2014-12-10 14:34:28 -0500

    Refactoring.

---
+++ classes/simulation.py
@@ -0,0 +1,58 @@
+"""simulation.py: a class that represents a simulation, storing all atomic information and simulation parameters"""
+
+## System imports
+import random
+import math
+
+## Local imports
+from classes import atom
+
+class Simulation:
+
+	# Constants involved in a simulation
+	kb = 1.380e-23
+
+	# Simulation/atomic parameters
+	m = (39.95*1.6747e-24) # mass of a single atom , Kg
+	e = kb*120 # depth of potential well, J
+	d = 3.4 # sigma in Lennard-Jones Potential ,angstroms
+	rcut = 2.25*d # Cutoff radius , angstroms
+	T = 120 # Temperature, K
+
+	# List of atoms
+	numAtoms = 0
+	atoms = []
+
+	def __init__(self, numAtoms):
+		"""Create a simulation with numAtoms"""
+		self.numAtoms = numAtoms
+		for i in range(0,numAtoms):
+			self.atoms.append(atom)
+	
+	def randomizePositions(self):
+		"""Give each atom random positions within the box"""
+		for atom in self.atoms:
+			atom.x = random.random()*34.78
+			atom.y = random.random()*34.78
+			atom.z = random.random()*34.78
+	
+	def applyBoltzmannDist(self):
+		"""Apply Boltzmann distribution to atomic velocities"""
+		normDist = []
+		scaling_factor = math.sqrt(self.kb*self.T/self.m)
+
+		# Establish Normal Distribution
+		for i in range(0,3*self.numAtoms):
+			normDist.append(random.gauss(0,1))
+	
+		# Apply scaling factor to distribution
+		for number in normDist:
+			number = number*scaling_factor
+	
+		# Distribute velocities	
+		rand_index = 0
+		for atom in self.atoms:
+			atom.vx = normDist[rand_index]
+			atom.vy = normDist[rand_index]
+			atom.vz = normDist[rand_index]
+			rand_index = rand_index + 3
--- simulate.py
+++ simulate.py
@@ -1,44 +1,20 @@
 ## System imports
 import sys
-import random
 import math
 
 ## Local imports
-from modules import atomicProperties as ap
-from classes import atom
+from classes.simulation import Simulation
 
-# Create 864 atom objects, store in list.
-num_particles = 864
-atoms = []
-for i in range(0,864):
-	atoms.append(atom)
+# Instanstiate a new Simulation Object, with 864 particles.
+sim = Simulation(864)
 
 # Give each atom a random position in 3D-space
-for atom in atoms:
-	atom.x = random.random()*34.78
-	atom.y = random.random()*34.78
-	atom.z = random.random()*34.78
+sim.randomizePositions()
 	
-# Generate 3N (N=864) random numbers from normal distribution.
-norm_dist = []
-for i in range(0,3*num_particles):
-	norm_dist.append(random.gauss(0,1))
-
-# Apply scaling factor (sqrt(kT/m))
-scaling_factor = math.sqrt(ap.kb*ap.T/ap.m)
-for number in norm_dist:
-	number = number*scaling_factor
-
 # Apply random velocities to particles.
-rand_index = 0
-for atom in atoms:
-	atom.vx = norm_dist[rand_index]
-	atom.vy = norm_dist[rand_index+1]
-	atom.vz = norm_dist[rand_index+2]
-	rand_index = rand_index+3
+sim.applyBoltzmannDist()
 
 # Allow them to move according to some time step
-
 
 # Integrate, find new positions and momenta
 
