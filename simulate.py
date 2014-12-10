aeb7fd4a14f42fcf35e217b29f49a480076708a7
Ken Newcomb <KenNewcombNJ@gmail.com>
2014-12-09 23:02:19 -0500

    Apply Boltzmann distribution to velocities

--- simulate.py
+++ simulate.py
@@ -20,14 +20,22 @@ for atom in atoms:
 	atom.z = random.random()*34.78
 	
 # Generate 3N (N=864) random numbers from normal distribution.
-normal_dist = []
+norm_dist = []
 for i in range(0,3*num_particles):
-	normal_dist.append(random.gauss(0,1))
+	norm_dist.append(random.gauss(0,1))
 
 # Apply scaling factor (sqrt(kT/m))
 scaling_factor = math.sqrt(ap.kb*ap.T/ap.m)
-for number in normal_dist:
+for number in norm_dist:
 	number = number*scaling_factor
+
+# Apply random velocities to particles.
+rand_index = 0
+for atom in atoms:
+	atom.vx = norm_dist[rand_index]
+	atom.vy = norm_dist[rand_index+1]
+	atom.vz = norm_dist[rand_index+2]
+	rand_index = rand_index+3
 
 # Allow them to move according to some time step
 
